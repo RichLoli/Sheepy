d57f662c6d6cc67a51b9463ab41df766605ff86c
Aya <loligafeo@gmail.com>
2022-01-14 17:31:21 +0800

    仓库初始化

---
+++ src/main/java/com/macoloco/sheepy/SheepyApplication.java
@@ -0,0 +1,13 @@
+package com.macoloco.sheepy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheepyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheepyApplication.class, args);
+    }
+
+}
